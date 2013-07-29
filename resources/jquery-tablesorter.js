204b72db4fa1298c7fde78d16f5c5b8e2560ce1b
Christopher Brown <io@henrian.com>
2013-07-29 14:58:12 -0500

    Add tablesorter images, only available on github

--- resources/jquery-tablesorter.js
+++ resources/jquery-tablesorter.js
@@ -6,26 +6,39 @@ module.exports = function(version, callback) {
   // the versions that cloudflare exposes are weird, 2.5.2 and 2.9.1, but since
   // the homepage doesn't expose versioned sources, I go with the CDN.
   var cloudflare = cdn.cloudflare('jquery.tablesorter')(version);
-  var github = cdn.github('Mottie/tablesorter')('v' + version);
+  var github_base = cdn.github('Mottie/tablesorter');
+  var github_js = github_base('v' + version + '/js');
+  var github_images = github_base('v' + version + '/css/images');
+  var cdns = [github_js, cloudflare];
 
   // cdnjs doesn't provide the css or gifs, unfortunately
   var filename_urls = {
-    'jquery-tablesorter.js': [
-      github('js/jquery.tablesorter.js'),
-      cloudflare('jquery.tablesorter.js'),
-    ],
-    'jquery-tablesorter.min.js': [
-      github('js/jquery.tablesorter.min.js'),
-      cloudflare('jquery.tablesorter.min.js'),
-    ],
-    'jquery-tablesorter.widgets.js': [
-      github('js/jquery.tablesorter.widgets.js'),
-      cloudflare('jquery.tablesorter.widgets.js'),
-    ],
-    'jquery-tablesorter.widgets.min.js': [
-      github('js/jquery.tablesorter.widgets.min.js'),
-      cloudflare('jquery.tablesorter.widgets.min.js'),
-    ],
+    'jquery-tablesorter.js': cdn.mapApply(cdns, 'jquery.tablesorter.js'),
+    'jquery-tablesorter.min.js': cdn.mapApply(cdns, 'jquery.tablesorter.min.js'),
+    'jquery-tablesorter-widgets.js': cdn.mapApply(cdns, 'jquery.tablesorter.widgets.js'),
+    'jquery-tablesorter-widgets.min.js': cdn.mapApply(cdns, 'jquery.tablesorter.widgets.min.js'),
   };
+
+  [
+    'black-desc.gif',
+    'black-unsorted.gif',
+    'dropbox-asc-hovered.png',
+    'dropbox-asc.png',
+    'dropbox-desc-hovered.png',
+    'dropbox-desc.png',
+    'green-asc.gif',
+    'green-desc.gif',
+    'green-header.gif',
+    'green-unsorted.gif',
+    'ice-asc.gif',
+    'ice-desc.gif',
+    'ice-unsorted.gif',
+    'white-asc.gif',
+    'white-desc.gif',
+    'white-unsorted.gif',
+  ].forEach(function(image_name) {
+    filename_urls['img/' + image_name] = [github_images(image_name)];
+  });
+
   callback(null, filename_urls);
 };
