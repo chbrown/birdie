782e1158f185692f560b73d2ef39649310183329
Christopher Brown <io@henrian.com>
2013-09-08 18:46:40 -0500

    Update _ to 1.5.2; reformat a couple files.

--- resources/backbone.js
+++ resources/backbone.js
@@ -1,8 +1,9 @@
 var cdn = require('../cdn');
 module.exports = function(version, callback) {
-  var versions = ['1.0.0', '0.9.9', '0.9.2', '0.9.1', '0.9.0', '0.5.3', '0.5.2',
-    '0.5.1', '0.5.0', '0.3.3', '0.3.2', '0.3.1', '0.3.0', '0.2.0', '0.1.2',
-    '0.1.1', '0.1.0'];
+  var versions = [
+    '1.0.0',
+    '0.9.9', '0.9.2', '0.9.1', '0.9.0', '0.5.3', '0.5.2', '0.5.1', '0.5.0',
+    '0.3.3', '0.3.2', '0.3.1', '0.3.0', '0.2.0', '0.1.2', '0.1.1', '0.1.0'];
   version = {'*': '1.0.0'}[version] || version;
   if (versions.indexOf(version) < 0)
     console.error('That version is not officially supported.');
